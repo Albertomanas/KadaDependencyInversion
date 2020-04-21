6b88c9ba9da5afb6a5e9c86544d3e4a4b87f17ec
Albertomanas <amanas@cifpfbmoll.eu>
2020-04-21 21:38:14 +0200

    refactor(InventarioBBDD): Implementada interfaz Inventario

--- KataDI/src/main/java/org/formacion/di/bbdd/InventarioBBDD.java
+++ KataDI/src/main/java/org/formacion/di/bbdd/InventarioBBDD.java
@@ -1,12 +1,19 @@
 package org.formacion.di.bbdd;
 
 
-public class InventarioBBDD {
+public class InventarioBBDD implements Inventario{
 
 
     // Metodo de consulta a la base de datos
 
-    public int numeroProductos(String tienda, String producto) {
+    /**
+     *
+     * @param tienda
+     * @param producto
+     * @return
+     */
+    @Override
+     public int numeroProductos(String tienda, String producto) {
         return BBDD.stocs.get(tienda).get(producto);
     }
 }
